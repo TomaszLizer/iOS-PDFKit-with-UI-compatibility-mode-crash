c32ad26260c83e482d64f4486cc444f3bdb2ac87
TomaszLizer <tomasz.lizer@brightinventions.pl>
2025-12-02 08:04:37 +0100

    Initial Commit

---
+++ Test-PDFKit-SwiftUI/Test_PDFKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_PDFKit_SwiftUIApp.swift
+//  Test-PDFKit-SwiftUI
+//
+//  Created by Tomasz Lizer on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test_PDFKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
